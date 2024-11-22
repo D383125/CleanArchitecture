35bfb3667e791a89361b86578f8a6a1a44b68e4e
Brian Mooney <breen25@hotmail.com>
2024-11-22 17:28:09 +1000

    DEV-01: Initial Submission

---
+++ src/ApplicationCore/Specifications/Specification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainCore.Specifications
+{
+    internal class Specification
+    {
+        //Use Query Sepcifiction - addInlcude etc
+        //so linq query syntact is not littered everywhere
+        //Also facilates Opne-closed in Data Access like Repositry pattern as it doesnt need to change - just add gthe specification
+
+    }
+}
---
+++ src/WebApp/Contracts/CreateProductRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Contracts;
+
+public class CreateProductRequest
+{
+    [Required]
+    public string Name { get; set; }
+
+    public decimal Price { get; set; }
+}
